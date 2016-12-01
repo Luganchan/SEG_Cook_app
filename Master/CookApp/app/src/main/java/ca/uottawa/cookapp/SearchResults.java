94f531dfd575216108f732fc4db484b52f1d022d
KingOfLinux <shawnco789@gmail.com>
2016-12-01 12:08:52 -0500

    image bug fixed and the search recipe crash

--- Master/CookApp/app/src/main/java/ca/uottawa/cookapp/SearchResults.java
+++ Master/CookApp/app/src/main/java/ca/uottawa/cookapp/SearchResults.java
@@ -10,13 +10,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.Serializable;
+
 import static java.security.AccessController.getContext;
 
 /**
  * Created by Kevin on 2016/11/30.
  */
 
-public class SearchResults extends AppCompatActivity {
+public class SearchResults extends AppCompatActivity implements Serializable {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +32,16 @@ public class SearchResults extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-                Recipe item = (Recipe) parent.getItemAtPosition(position);
-                openRecipe(item);
+                Intent intent = new Intent (getApplicationContext(),RecipeActivity.class);
+                intent.putExtra("recipeIndex",position);
+                startActivityForResult(intent,0);
             }
         });
     }
 
     public void openRecipe(Recipe item){
         Intent intent = new Intent(getApplicationContext(), RecipeActivity.class);
-        intent.putExtra("Recipe", item);
+        intent.putExtra("recipeIndex", item);
         startActivity(intent);
     }
 }
