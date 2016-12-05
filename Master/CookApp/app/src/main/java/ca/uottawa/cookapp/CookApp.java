8f59a41b314e9e5f5a4b198b1b85cd9a94e4382d
KingOfLinux <shawnco789@gmail.com>
2016-12-05 16:46:48 -0500

    trying to pull

--- Master/CookApp/app/src/main/java/ca/uottawa/cookapp/CookApp.java
+++ Master/CookApp/app/src/main/java/ca/uottawa/cookapp/CookApp.java
@@ -60,7 +60,7 @@ public class CookApp extends AppCompatActivity implements SearchView.OnQueryText
 
 
         //RecipeManager recipeManager = new RecipeManager();
-        Recipe pasta = new Recipe(1,ContextCompat.getDrawable(getApplicationContext(), R.drawable.pasta), "Pasta", new String[]{"Water", "Bread", "Butter", "tomatoes", "shit" , "fuck", "Crap"});
+        Recipe pasta = new Recipe(1,ContextCompat.getDrawable(getApplicationContext(), R.drawable.pasta), "Pasta", new String[]{"Water", "Bread", "Butter"});
 
         Recipe soup = new Recipe(1,ContextCompat.getDrawable(getApplicationContext(), R.drawable.soupe), "Soup",  new String[]{"Water", "Bread", "Butter", "tomatoes"});
         Recipe bread = new Recipe(2,ContextCompat.getDrawable(getApplicationContext(), R.drawable.bread), "Bread", new String[]{"Water", "Bread", "Butter", "tomatoes"});
